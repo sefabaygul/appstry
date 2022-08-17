72d02eacd25c91dcf665c528604fb4c2a7f154f4
Sefa Baygül <sefa.baygul06@gmail.com>
2022-08-17 14:53:57 +0300

    v3

--- src/C1.java
+++ src/C1.java
@@ -2,6 +2,7 @@ public class C1 {
 
     public static void main(String[] args) {
         System.out.println("app deneme 1");
+        System.out.println("app deneme 2");
 
     }
 }
---
+++ src/C2.java
@@ -0,0 +1,6 @@
+public class C2 {
+
+    public static void main(String[] args) {
+        System.out.println("C2 app deneme 1");
+    }
+}
