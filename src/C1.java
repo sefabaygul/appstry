4f456ea6c60aeb3564fcffdf7915da4798a99482
Sefa Baygül <sefa.baygul06@gmail.com>
2022-08-17 18:32:34 +0300

    v7aktif

--- src/C1.java
+++ src/C1.java
@@ -2,9 +2,5 @@ public class C1 {
 
     public static void main(String[] args) {
         System.out.println("app deneme 1");
-        System.out.println("app deneme 2");
-
-        int num2 = 10;
-
     }
 }
